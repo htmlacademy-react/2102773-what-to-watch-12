023498f3c53e85b7a4f1f83935903749d4938899
gerbertwest <gerbert.west@gmail.com>
2023-03-02 22:01:56 +0300

    2.16. Начинаем программировать (часть 1)

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,19 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainScreen from '../../pages/main-screen/main-screen';
+
+type AppScreenProps = {
+  filmCardTitle: string;
+  filmCardGenre: string;
+  filmCardYear: number;
+}
+
+function App(props: AppScreenProps): JSX.Element {
+  return (
+    <MainScreen
+      filmCardTitle={props.filmCardTitle}
+      filmCardGenre={props.filmCardGenre}
+      filmCardYear={props.filmCardYear}
+    />
+  );
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const Setting = {
+  filmCardTitle: 'The Grand Budapest Hotel',
+  filmCardGenre: 'Drama',
+  filmCardYear: 2014,
+} as const;
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App
+      filmCardTitle={Setting.filmCardTitle}
+      filmCardGenre={Setting.filmCardGenre}
+      filmCardYear={Setting.filmCardYear}
+    />
   </React.StrictMode>,
 );
